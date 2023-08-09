61edbd5c6a4218469c3197f207c2d1b7d2b19e78
alykrauze <alicekrauze@alices-MacBook-Pro.local>
2023-08-09 19:31:14 -0400

    adding csv

--- TABLES.sql
+++ TABLES.sql
@@ -1,14 +1,23 @@
 SELECT * FROM tshirts;
 
 
-DROP TABLE tshirts;
+DROP TABLE jeans;
 
-CREATE TABLE tshirts (
-	MM VARCHAR(50),
-	diesel VARCHAR(50),
-	blumarine VARCHAR(50)
+CREATE TABLE jeans (
+	MM6_jeans VARCHAR(50),
+	diesel_jeans VARCHAR(50),
+	blumarine_jeans VARCHAR(50)
 );
 
 SELECT * FROM jeans;
 
 SELECT * FROM tshirts;
+
+CREATE TABLE shirts_and_pants AS
+SELECT *
+FROM jeans
+CROSS JOIN tshirts;
+
+SELECT * FROM shirts_and_pants;
+
+COPY shirts_and_pants TO 'Users/alicekrauze/ru_bootcamp/homework/3Project/Project3_Group1' WITH CSV HEADER;

7a85803056f4a287ea92c6c49f898dce47a344b9
alykrauze <alicekrauze@alices-MacBook-Pro.local>
2023-08-09 16:58:39 -0400

    adding sql table

---
+++ TABLES.sql
@@ -0,0 +1,14 @@
+SELECT * FROM tshirts;
+
+
+DROP TABLE tshirts;
+
+CREATE TABLE tshirts (
+	MM VARCHAR(50),
+	diesel VARCHAR(50),
+	blumarine VARCHAR(50)
+);
+
+SELECT * FROM jeans;
+
+SELECT * FROM tshirts;
